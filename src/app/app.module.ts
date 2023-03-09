86627e67e5bbe6ce0ff85f2c6ddfde1273c07536
nkunic <45877400+nkunic@users.noreply.github.com>
2023-03-09 11:25:57 +0100

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
-  exports: [HeroesComponent, HeroDetailComponent],
+  imports: [
+    BrowserModule, 
+    FormsModule
+  ],
+  declarations: [
+    AppComponent, 
+    HeroesComponent, 
+    HeroDetailComponent,
+    MessagesComponent
+  ],
+  exports: [
+    HeroesComponent, 
+    HeroDetailComponent
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule {}
